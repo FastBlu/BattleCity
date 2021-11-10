533d379ba51955912efaa98f33fc6d904f1eaea5
Dima <qqq.blur@gmail.com>
2021-11-10 15:31:58 +0300

    Added texture

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -1,14 +1,19 @@
 #include "ResourceManager.h"
 #include "../Renderer/ShaderProgram.h"
+#include "../Renderer/Texture2D.h"
 #include <sstream>
 #include <fstream>
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#define STBI_ONLY_PNG
+#include "stb_image.h"
+
 
 ResourceManager::ResourceManager(const std::string& executablePath)
 {
 	size_t found = executablePath.find_last_of("/\\");
 	m_path = executablePath.substr(0, found);
-};
+}
 
 std::string ResourceManager::getFileString(const std::string& relativeFilePath) const
 {
@@ -23,7 +28,7 @@ std::string ResourceManager::getFileString(const std::string& relativeFilePath) 
 	std::stringstream buffer;
 	buffer << f.rdbuf();
 	return buffer.str();
-};
+}
 
 std::shared_ptr<Renderer::ShaderProgram> ResourceManager::loadShaders(const std::string& shaderName, const std::string& vertexPath, const std::string& fragmentPath)
 {
@@ -41,7 +46,7 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::loadShaders(const std:
 		return nullptr;
 	}
 
-	std::shared_ptr<Renderer::ShaderProgram>& newShader = m_ShaderPrograms.emplace(shaderName, std::make_shared<Renderer::ShaderProgram>(vertexString, fragmentString)).first->second;
+	std::shared_ptr<Renderer::ShaderProgram>& newShader = m_shaderPrograms.emplace(shaderName, std::make_shared<Renderer::ShaderProgram>(vertexString, fragmentString)).first->second;
 	if (newShader->isCompiled())
 	{
 		return newShader;
@@ -52,16 +57,50 @@ std::shared_ptr<Renderer::ShaderProgram> ResourceManager::loadShaders(const std:
 		<< "Fragment: " << fragmentPath << std::endl;
 
 	return nullptr;
-};
+}
 
 std::shared_ptr<Renderer::ShaderProgram> ResourceManager::getShaderProgram(const std::string& shaderName)
 {
-	ShaderProgramsMap::const_iterator it = m_ShaderPrograms.find(shaderName);
-	if (it != m_ShaderPrograms.end())
+	ShaderProgramsMap::const_iterator it = m_shaderPrograms.find(shaderName);
+	if (it != m_shaderPrograms.end())
 	{
 		return it->second;
 	}
 
 	std::cerr << "Can't find shader program: " << shaderName << std::endl;
 	return nullptr;
-};
+}
+
+std::shared_ptr<Renderer::Texture2D> ResourceManager::loadTexture(const std::string& textureName, const std::string& texturePath)
+{
+	int channels = 0;
+	int width = 0;
+	int height = 0;
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned char* pixels = stbi_load(std::string(m_path + "/" + texturePath).c_str(), &width, &height, &channels, 0);
+
+	if (!pixels)
+	{
+		std::cerr << "Can't load image: " << textureName << std::endl;
+		return nullptr;
+	}
+
+	std::shared_ptr<Renderer::Texture2D> newTexture = m_textures.emplace(textureName, std::make_shared<Renderer::Texture2D>(width, height, pixels, channels, GL_NEAREST, GL_CLAMP_TO_EDGE)).first->second;
+	stbi_image_free(pixels);
+	return newTexture;
+}
+
+std::shared_ptr<Renderer::Texture2D> ResourceManager::getTexture(const std::string& textureName)
+{
+	TexturesMap::const_iterator it = m_textures.find(textureName);
+	if (it != m_textures.end())
+	{
+		return it->second;
+	}
+
+	std::cerr << "Can't texture: " << textureName << std::endl;
+	return nullptr;
+}
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "Renderer/ShaderProgram.h"
 #include "Resources/ResourceManager.h"
+#include "Renderer/Texture2D.h"
 
 GLfloat points[] = {
     0.0f, 0.5f, 0.0f,
@@ -14,6 +15,12 @@ GLfloat colors[] = {
     1.0f, 0.0f, 0.0f,
     0.0f, 1.0f, 0.0f,
     0.0f, 0.0f, 1.0f
+};
+
+GLfloat texCoord[] = {
+    0.5f, 1.0f, 
+    1.0f, 0.0f, 
+    0.0f, 0.0f
 };
 
 int g_windowSizeX = 640;
@@ -85,6 +92,8 @@ int main(int argc, char** argv)
             return -1;
         }
 
+        auto tex = resourceManager.loadTexture("DefaultTexture", "res/textures/map_16x16.png");
+
         GLuint point_vbo = 0;
         glGenBuffers(1, &point_vbo);
         glBindBuffer(GL_ARRAY_BUFFER, point_vbo);
@@ -94,6 +103,11 @@ int main(int argc, char** argv)
         glGenBuffers(1, &color_vbo);
         glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
         glBufferData(GL_ARRAY_BUFFER, sizeof(colors), &colors, GL_STATIC_DRAW);
+
+        GLuint texCoord_vbo = 0;
+        glGenBuffers(1, &texCoord_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, texCoord_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(texCoord), &texCoord, GL_STATIC_DRAW);
 
         GLuint vao = 0;
         glGenVertexArrays(1, &vao);
@@ -107,6 +121,13 @@ int main(int argc, char** argv)
         glBindBuffer(GL_ARRAY_BUFFER, color_vbo);
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
+        glEnableVertexAttribArray(2);
+        glBindBuffer(GL_ARRAY_BUFFER, texCoord_vbo);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+        pDefaultShaderProgram->use();
+        pDefaultShaderProgram->setInt("tex", 0);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(pWindow))
         {
@@ -115,6 +136,7 @@ int main(int argc, char** argv)
 
             pDefaultShaderProgram->use();
             glBindVertexArray(vao);
+            tex->bind();
             glDrawArrays(GL_TRIANGLES, 0, 3);
 
             /* Swap front and back buffers */
